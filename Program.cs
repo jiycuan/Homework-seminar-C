a70f7e321e8d713eef5943cb7e6cc63c28eb4f53
Сорокин Алексей <jiysuan@mail.ru>
2022-07-10 02:01:45 +0400

    Сделал задачу 4, она же вторая по счету

--- Program.cs
+++ Program.cs
@@ -1,15 +1,62 @@
-﻿Console.Write ("Введите цифру: ");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.Write ("Введите ещё одну цифру: ");
-int b = Convert.ToInt32(Console.ReadLine());
-int max;
+﻿// Программа, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
 
-if (a > b)
+/*
+Console.Write ("Введите цифру: ");
+int number_a = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите ещё одну цифру: ");
+int number_b = Convert.ToInt32(Console.ReadLine());
+int max, min;
+
+if (number_a == number_b)
     {
-        max = (a);
+        Console.WriteLine("Числа равны");
     }
 else 
     {
-        max = (b);
+    if (number_a > number_b)
+    {
+        max = (number_a);
+        min = (number_b);
     }
-Console.WriteLine(max);
+    else 
+    {
+        max = (number_b);
+        min = (number_a);
+    }
+    
+    Console.Write("Максимальное число: " + max + ", ");
+    Console.Write("Минимальное число: " + min);
+    Console.WriteLine();
+    }
+*/
+
+
+
+// Программа, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+/*
+Console.Write ("Введите цифру: ");
+int number_a = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите ещё одну цифру: ");
+int number_b = Convert.ToInt32(Console.ReadLine());
+Console.Write ("И введите последнюю цифру: ");
+int number_c = Convert.ToInt32(Console.ReadLine());
+int max;
+
+if (number_a > number_b)
+    {
+        max = number_a;
+    }
+else 
+    {
+        max = number_b;
+    }
+    if (max > number_c)
+        {
+        }
+    else
+        {
+            max = number_c;
+        }
+Console.WriteLine("Максимальное число: " + max);
+*/
+

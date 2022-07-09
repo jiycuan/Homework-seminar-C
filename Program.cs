5915c71b17560aece5d660919e9254a2fd26ce46
Сорокин Алексей <jiysuan@mail.ru>
2022-07-10 02:50:14 +0400

    Сделал все задания и превратил код в комментарий

--- Program.cs
+++ Program.cs
@@ -60,3 +60,43 @@ else
 Console.WriteLine("Максимальное число: " + max);
 */
 
+
+
+// Программа, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+/*
+Console.Write ("Введите цифру: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if ((number % 2) == 0)
+    {
+        Console.WriteLine("Число чётное");
+    }
+else
+    {
+        Console.WriteLine("Число нечётное");
+    }
+*/
+
+
+
+// Программа, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+/*
+Console.Write ("Введите цифру: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int temp = 2;
+
+if (N < 1)
+    {
+        Console.WriteLine("В условии же написано - от 1 до N, а значит N не может быть меньше единицы, умник!");
+    }
+else
+    {
+
+     while (N > temp)
+     {
+        Console.Write(temp + ", ");
+        temp = (temp + 2);
+     }
+    }
+Console.WriteLine();
+*/

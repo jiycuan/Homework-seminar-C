112cb5c1b0b611428c556ba4a8a5956c24d86e54
Сорокин Алексей <jiysuan@mail.ru>
2022-07-10 00:46:44 +0400

    Выполнил задачу 2

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write ("Введите цифру: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите ещё одну цифру: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int max;
+
+if (a > b)
+    {
+        max = (a);
+    }
+else 
+    {
+        max = (b);
+    }
+Console.WriteLine(max);
